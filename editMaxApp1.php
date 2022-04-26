dc21860aa4bf87669e8663be41f753ea43a9b9c2
sara81a <alsalehsara6@gmail.com>
2022-04-26 13:59:30 -0700

    edit

---
+++ editMaxApp1.php
@@ -0,0 +1,211 @@
+<?php
+
+$host="localhost";   $duus="root";
+$dbp="";     $dbname="webs";
+if(!($db2=mysqli_connect($host,$duus,$dbp,$dbname)))
+die("<p>the connection error</p>");
+
+if(!isset($_GET['id'] )){
+    die('id not provided');
+}
+$id=$_GET['id'];
+
+$query="SELECT * FROM  setappointment  where AppointmentID= '$id' ";
+
+
+
+
+if(!($result=mysqli_query($db2,$query)))
+die("<p>the qurey error</p>");
+
+
+$row=mysqli_fetch_array($result);
+//mysqli_free_result($result);
+//mysqli_close($db2);
+//print_r($pizzas);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+       
+       <meta charset="UTF-8">
+       <title>editMaxApp1 </title>
+       <link rel="stylesheet" href="editStyle.css">
+       <link rel="stylesheet" href="Header and Footer.css">
+      <script src="https://kit.fontawesome.com/493718cddd.js" crossorigin="anonymous"></script>
+       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"rel="stylesheet"/> 
+
+       <script src="https://kit.fontawesome.com/493718cddd.js" crossorigin="anonymous"></script>
+   
+    </head>
+    <body>
+           <header>
+       
+      <a href="./Home Manager.html" class="logo"><img src="logo.png" alt="logo" height="50rem" >
+        <span style="font-size: 40px;">WEBETS</span></a>
+            <input type="checkbox" id="menu-bar">
+            <label for="menu-bar" class="fas fa-bars"></label>
+        
+            <nav class="navbar">
+                <ul class="nav-list">
+                    <li  ><a href="home-manager.html">Home</a>
+                       <!-- <ul class="sub-menu" id="sub-menu-arrow"> 
+                          <li > <a href="./MahaB Add A Service Page.html">Add a New Service</a></li>
+                          <li><a href="./availabel apointment manager.html">Set a New Appointment</a></li>
+                          <li><a href="./request list manager.html">View Requests List</a></li>
+          
+                          <li><a href="./upcoming and previous manager.html">View Appointments List</a> </li>
+                  
+                        </ul>-->
+                      </li>
+                      
+                      
+                   <li><a href="p1.html">Services</a></li> 
+
+                   <li><a href="AboutusM.html">About Us</a></li> 
+                    <li class="move-right-btn" ><a href="main.html"id="profile"><i class="fas fa-sign-out" ></i></a>
+                       <!--  <ul class="sub-menu" id="sub-menu-arrow2"> 
+                            <li ><a href="#">View Profile</a></li>
+                            <li><a href="./LnadingPage.html">Sign Out</a></li>
+                    
+                          </ul>-->
+                        </li>
+                  </ul>
+                
+                <!-- ****if you're working on a pet owner view replace <i class="fa-solid fa-user-doctor"> with <i class="fa-solid fa-user"></i>  -->
+        
+        
+            </nav>
+    
+    </header>
+    <section>
+        <!--<div class="ooo"><a href="max.html">Back</a></div>-->
+       
+      
+        <div class="wrapper bg-white mt-sm-5">
+            <h4 class="pb-4 border-bottom" style="font-size: 40px; color: #ADD8E6; text-align: center; ">APPOINTMENT SETTINGS FOR MAX</h4>
+
+            
+           <!-- <div class="iimg">
+                <img src="n.png" alt="user" >
+           
+                 
+                </div>
+                <br><br>-->
+            <form  class="inputA" method = "POST" action="editMaxApp1.php?id=<?php echo $id; ?>" >
+                
+            <div class="py-2">
+            <div class="row py-2">
+                    <div class="col-md-6 pt-md-0 pt-3"> <label for="Date"> Date</label> <input type="date" class="bg-light form-control"  value="<?php echo $rows['date']; ?>"name="date"  required> </div>
+               
+                </div></div>
+                <div class="row py-2">
+                    <div class="col-md-6"> <label for="country">service</label> <select name="country" id="country" class="bg-light"required>
+                            <option value="DENTAL CARE" >DENTAL CARE</option>
+                            <option value="HAIR CUT"selected>HAIR CUT</option>
+                            <option value="Grooming"selected>Grooming</option>
+                        
+                    </div>
+                        </select> </div>
+                        <div class="row py-2">
+                            <div class="col-md-6"> <label for="country">Time</label> <select name="country" id="country" class="bg-light"required>
+                                    <option value="Female" selected>4:30</option>
+                                    <option value="Male">7:15</option>
+                                    <option value="Male">10:00</option>
+                                    <option value="Male">11:40</option>
+                            </div>
+                                </select> </div>
+               
+
+
+
+
+
+
+
+
+
+
+
+               <!-- <div class="row py-2">
+                    <div class="col-md-6"> <label for="email">Service</label> <input type="text" class="bg-light form-control" value="Lionhead" placeholder="Pet's breed"required> </div>
+
+                </div>-->
+                <div class="row py-2">
+                    <div class="col-md-6"> <label for="country">service</label> <select name="country" id="country" class="bg-light"required>
+                            <option value="DENTAL CARE" >DENTAL CARE</option>
+                            <option value="HAIR CUT"selected>HAIR CUT</option>
+                            <option value="Grooming"selected>Grooming</option>
+                        
+                    </div>
+                        </select> </div>
+                     <!--   <div class="row py-2">
+                            <div class="col-md-6"> <label for="country">Time</label> <select name="country" id="country" class="bg-light"required>
+                                    <option value="Female" selected>4:30</option>
+                                    <option value="Male">7:15</option>
+                                    <option value="Male">10:00</option>
+                                    <option value="Male">11:40</option>
+                            </div>
+                                </select> </div>-->
+                                <div class="py-2">
+                           <div class="row py-2">
+                    <div class="col-md-6 pt-md-0 pt-3"> <label for="lastname"> Time</label> <input type="time" class="bg-light form-control" required> </div>
+                               </div></div>
+               
+                               <div class="py-2">
+                           <div class="row py-2">
+                    <div class="col-md-6 pt-md-0 pt-3"> <label for="lastname"> Note</label> <input type="text" class="bg-light form-control" required> </div>
+                               </div></div>
+                        
+
+
+
+                            </form>
+                          </div>
+<br><br>
+                <div class="py-3 pb-4 border-bottom" > <button class="btn btn-primary mr-3" >Save</button> <button class="btn border button">Cancel</button> </div>
+               
+            </div>
+        </div>
+       <!-- <div class="pic"><img src="IMG_3875-removebg-preview.png" alt=""></div>
+        <div class="pic2"><img src="m.png" alt=""></div>-->
+    </section>
+    <div class="footer">
+        <div class="box-container">
+            
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="./Home Manager.html">Home</a>
+                <a href="./Services Manager.html">Services</a>
+                <a href="./About Us Manager.html">About US</a>    
+            </div>
+            <div class="box">
+                <h3>Find Us</h3>  
+               
+                <div class="info">
+                    
+                    <i class="fas fa-envelope"></i>
+                                <a href="mailto:StudentID@student.ksu.edu.sa">Email</a>
+                </div>
+               
+            </div>
+            <div class="box">
+                <h3>Contact Information</h3>
+                <div class="info">
+                    <i class="fas fa-phone"></i>
+                    <p>+123-456-789 <br> +111-222-3333</p>
+                </div>
+                    <div class="info">
+                        <i class="fas fa-envelope"></i>
+                        <p>costumerService@Healed.com <br> costumerService2@Healed.com</p>
+                    </div>
+                   
+            </div>
+            
+        </div>
+        <h1 class="credit">&copy; copyright @ 2022 by software Engineers</h1>
+    </div>
+    
+    </body>
+</html>
